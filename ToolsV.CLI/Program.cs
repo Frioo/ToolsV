9d4f380cfe540bf235da8eb401e10ce23931a1b7
Adam Żbikowski <ajzbikowski@gmail.com>
2024-01-05 00:26:41 +0100

    Create CLI project for lib testing

--- ToolsV.CLI/Program.cs
+++ ToolsV.CLI/Program.cs
@@ -13,11 +13,21 @@ namespace ToolsV.CLI
             if (manager == null)
             {
                 Console.WriteLine("GTA V installation directory was not found. You might be missing some registry keys.");
+                return;
             }
 
             Console.WriteLine($"Patch: {manager?.GameInfo.Version}");
             Console.WriteLine($"Installed at: {manager?.GameInfo.Path}");
             Console.WriteLine($"Edition: {manager?.GameInfo.Edition}");
+            Console.WriteLine($"Language: {manager?.GameInfo.Language}");
+            Console.WriteLine($"Write-protected: " + (manager?.GameInfo.IsWriteProtected == true ? "Yes" : "No"));
+
+            var mods = manager.ModManager.GetMods();
+            Console.WriteLine($"Mods: {mods.Count}");
+            foreach (var mod in mods)
+            {
+                Console.WriteLine($"- {mod.Filename} ({mod.Type})");
+            }
         }
     }
 }
